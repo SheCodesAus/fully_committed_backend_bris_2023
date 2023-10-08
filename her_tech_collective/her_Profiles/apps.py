8a4dcd6094bdd205ce7c8fe373809ab0142377b4
Nsc287 <xzlynb287@gmail.com>
2023-10-08 14:22:36 +1000

    app_herProfiles initial commit with GET POST PUT DELETE configured. Next stage local API testing.

--- her_tech_collective/her_Profiles/apps.py
+++ her_tech_collective/her_Profiles/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class HerProfilesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'her_Profiles'
+    name = 'her_profiles'
