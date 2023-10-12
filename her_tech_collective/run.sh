ab325ac9160007dfba9262d83d1c78e7dbeed0f9
Nsc287 <xzlynb287@gmail.com>
2023-10-12 12:17:23 +1000

    run.sh setting updates

--- her_tech_collective/run.sh
+++ her_tech_collective/run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 python manage.py migrate
 python manage.py createsuperuser --no-input
-gunicorn --bind :8000 --workers 1 hertechcollective.wsgi
+gunicorn --bind :8000 --workers 1 her_tech_collective.wsgi
