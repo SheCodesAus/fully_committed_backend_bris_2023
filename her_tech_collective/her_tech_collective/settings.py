e469cfcce0c0ce957db9b3958eed11cdba2afb61
Leah <ldctaylor@gmail.com>
2023-10-16 20:34:01 +1100

    Add api-auth-token

--- her_tech_collective/her_tech_collective/settings.py
+++ her_tech_collective/her_tech_collective/settings.py
@@ -58,6 +58,13 @@ INSTALLED_APPS = [
 
 AUTH_USER_MODEL = 'users.CustomUser'
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ]
+
+}
+
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
