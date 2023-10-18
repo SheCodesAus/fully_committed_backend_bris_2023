fcc9aab7573099b7f18d5e1c87fb2b99e5ca1b3e
Nsc287 <xzlynb287@gmail.com>
2023-10-18 22:30:59 +1000

    users url added path('<str:username>/', views.CustomUserByUsername.as_view())

--- her_tech_collective/users/urls.py
+++ her_tech_collective/users/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('register/', views.CustomUserList.as_view()),
     path('<int:pk>/', views.CustomUserDetail.as_view()),
+    path('<str:username>/', views.CustomUserByUsername.as_view())
 ]
